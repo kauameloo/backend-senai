c143329d1a63875edbfe65efa79963e32678451a
Kauã Rodrigues <kaua.rodrigues2620@gmail.com>
2023-04-03 17:22:38 -0300

    finalizada atividades de switch-case

---
+++ Estrutura Switch Case/estruturas-switch-case/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine($"Informe o número correspondente ao dia da semana - exemplo 2 para segunda");
+
+int diaDaSemana = int.Parse(Console.ReadLine()!);
+
+switch (diaDaSemana)
+{
+    case 1:
+        Console.WriteLine($"DOMINGOU");
+        break;
+    case 2:
+        Console.WriteLine($"SEGUNDOU");
+        break;
+    case 3:
+        Console.WriteLine($"TERÇOU");
+        break;
+    case 4:
+        Console.WriteLine($"QUARTOU");
+        break;
+    case 5:
+        Console.WriteLine($"QUINTOU");
+        break;
+    case 6:
+        Console.WriteLine($"SEXTOU");
+        break;
+    case 7:
+        Console.WriteLine($"SABADOU");
+        break;
+
+    default:
+        Console.WriteLine($"O dia informado não corresponde com nenhum dia da semana.");
+        break;
+}
+
---
+++ Estrutura Switch Case/refrigerante/Program.cs
@@ -0,0 +1,106 @@
+﻿//Coca cola
+//Pepsi
+//Fanta
+//Monster
+
+//crie um menu de opções e peça para o usuário escolher uma das opções
+//a depender da opção, exiba uma mensagem se gostaria de acrescentar gelo
+//se sim, exiba uma mensagem que a bebida em questão será com adicional de gelo
+//caso contrário, a bebida é sem gelo adicional
+
+
+Console.WriteLine(@$"Escolha uma opção de bebida: 
+1- Coca-cola
+2- Pepsi
+3- Fanta
+4- Monster
+");
+
+int refri = int.Parse(Console.ReadLine()!);
+int gelo = 0;
+
+switch (refri)
+{
+    case 1:
+        Console.WriteLine($"Você escolheu coca-cola");
+        Console.WriteLine(@$"Gostaria de adicionar gelo?
+        1- Com gelo
+        2- Sem gelo");
+        gelo = int.Parse(Console.ReadLine()!);
+        switch (gelo)
+        {
+            case 1:
+                Console.WriteLine($"Você escolheu uma coca-cola com gelo.");
+                break;
+            case 2:
+                Console.WriteLine($"Você escolheu uma coca-cola sem gelo.");
+                break;
+            default:
+                Console.WriteLine($"Escolha inválida.");
+                break;
+        }
+
+
+        break;
+    case 2:
+        Console.WriteLine($"Você escolheu Pepsi");
+        Console.WriteLine(@$"Gostaria de adicionar gelo?
+        1- Com gelo
+        2- Sem gelo");
+        gelo = int.Parse(Console.ReadLine()!);
+        switch (gelo)
+        {
+            case 1:
+                Console.WriteLine($"Você escolheu uma coca-cola com gelo.");
+                break;
+            case 2:
+                Console.WriteLine($"Você escolheu uma coca-cola sem gelo.");
+                break;
+            default:
+                Console.WriteLine($"Escolha inválida.");
+                break;
+        }
+        break;
+    case 3:
+        Console.WriteLine($"Você escolheu Fanta");
+        Console.WriteLine(@$"Gostaria de adicionar gelo?
+        1- Com gelo
+        2- Sem gelo");
+        gelo = int.Parse(Console.ReadLine()!);
+        switch (gelo)
+        {
+            case 1:
+                Console.WriteLine($"Você escolheu uma coca-cola com gelo.");
+                break;
+            case 2:
+                Console.WriteLine($"Você escolheu uma coca-cola sem gelo.");
+                break;
+            default:
+                Console.WriteLine($"Escolha inválida.");
+                break;
+        }
+        break;
+    case 4:
+        Console.WriteLine($"Você escolheu Monster");
+        Console.WriteLine(@$"Gostaria de adicionar gelo?
+        1- Com gelo
+        2- Sem gelo");
+        gelo = int.Parse(Console.ReadLine()!);
+        switch (gelo)
+        {
+            case 1:
+                Console.WriteLine($"Você escolheu uma coca-cola com gelo.");
+                break;
+            case 2:
+                Console.WriteLine($"Você escolheu uma coca-cola sem gelo.");
+                break;
+            default:
+                Console.WriteLine($"Escolha inválida.");
+                break;
+        }
+        break;
+    default:
+        Console.WriteLine($"Escolha inválida.");
+        break;
+}
+
