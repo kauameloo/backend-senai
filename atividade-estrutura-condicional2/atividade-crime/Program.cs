f557e76f8d50d92b1596ecedfe977b24070f5931
Kauã Rodrigues <kaua.rodrigues2620@gmail.com>
2023-04-03 15:08:52 -0300

    finalizado exercicios da lista 2

--- atividade-estrutura-condicional2/atividade-crime/Program.cs
+++ atividade-estrutura-condicional2/atividade-crime/Program.cs
@@ -14,21 +14,63 @@
 
 // O programa deve no final emitir uma classificação sobre a participação da pessoa no crime. Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como “Culpado“. Caso contrário, ele será classificado como "Inocente“.
 
+int culpa = 0;
+
 Console.WriteLine($"Telefonou para vítima? ");
-string p1 = (Console.ReadLine()!.ToUpper());
+string r1 = Console.ReadLine()!.ToLower();
+if (r1 == "sim")
+{
+    culpa++;
+}
 
 Console.WriteLine($"Esteve no local do crime? ");
-string p2 = (Console.ReadLine()!.ToUpper());
+string r2 = Console.ReadLine()!.ToLower();
+if (r2 == "sim")
+{
+    culpa+= 1;
+}
 
 Console.WriteLine($"Mora perto da vítima? ");
-string p3 = (Console.ReadLine()!.ToUpper());
+string r3 = Console.ReadLine()!.ToLower();
+if (r3 == "sim")
+{
+    culpa+= 1;
+}
 
 Console.WriteLine($"Devia para a vítima? ");
-string p4 = (Console.ReadLine()!.ToUpper());
+string r4 = Console.ReadLine()!.ToLower();
+if (r4 == "sim")
+{
+    culpa+= 1;
+}
 
 Console.WriteLine($"Já trabalhou com a vítima? ");
-string p5 = (Console.ReadLine()!.ToUpper());
+string r5 = Console.ReadLine()!.ToLower();
+if (r5 == "sim")
 
+{
+    culpa+= 1;
+}
+if (culpa < 2)
+{
+    Console.WriteLine($"Você está classificado como Inocente.");
+}
+else if (culpa == 2)
+{
+    Console.WriteLine($"Você está classificado como suspeito.");
+}
+
+else if (culpa == 5)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Você é o culpado!");
+    Console.ResetColor();
+}
+
+else
+{
+    Console.WriteLine($"Você é um cumplice.");
+}
 
 
 
--- atividade-estrutura-condicional2/atividade-vogal/Program.cs
+++ atividade-estrutura-condicional2/atividade-vogal/Program.cs
@@ -1,2 +1,14 @@
 ﻿// Faça um programa que verifique se uma letra digitada é vogal ou consoante.
 
+Console.WriteLine($"Digite uma letra do alfabeto: ");
+char letra = char.Parse(Console.ReadLine()!.ToLower());
+
+if (letra == 'a' || letra == 'e'|| letra == 'i' || letra == 'o' || letra == 'u' )
+{
+    Console.WriteLine($"A letra escolhida foi uma vogal!");
+}
+else
+{
+    Console.WriteLine($"A letra escolhida foi uma consoante!");
+}
+
