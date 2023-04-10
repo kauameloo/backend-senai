40d7d2568ffa56b84ad155a7bcd3ff2f0c6c663c
Kauã Rodrigues <kaua.rodrigues2620@gmail.com>
2023-04-10 15:28:15 -0300

    atividades de estrutura de repeticao finalizadas

---
+++ Estrutura Repeticao/fibonacci-desafio/Program.cs
@@ -0,0 +1,12 @@
+﻿int f;
+int numeroAnterior = 0;
+int numeroAtual = 1;
+
+
+for (int i = 0; i < 10; i++)
+{
+    f = numeroAnterior + numeroAtual;
+    Console.WriteLine(f);
+    numeroAnterior = numeroAtual;
+    numeroAtual = f;
+}
--- Estrutura Repeticao/idade-atv-5/Program.cs
+++ Estrutura Repeticao/idade-atv-5/Program.cs
@@ -1,17 +1,106 @@
 ﻿// 5. Faça um programa que receba a idade, o peso e o sexo de 10 pessoas. Calcule e imprima:
-      
+
 // A.Total de Homens;      
 // B.Total de Mulheres;      
 // C.Média de idade dos Homens;      
 // D.Média de idade das mulheres.
 
-int h = 0, m = 0;
+int h = 0, m = 0, idade = 0, idadeH = 0, idadeM = 0;
+string genero = "";
+float peso = 0;
 bool respostaCerta = true;
 
-for (int pessoas = 1; pessoas <= 10; pessoas++)
+for (int pessoas = 1; pessoas <= 3; pessoas++)
 {
     do
     {
-        
+        Console.WriteLine($"Qual o seu gênero? masculino(m) / feminino(f)");
+        genero = Console.ReadLine()!;
+
+        switch (genero)
+        {
+            case "m":
+                respostaCerta = true;
+                h++;
+                break;
+            case "f":
+                respostaCerta = true;
+                m++;
+                break;
+            default:
+                Console.WriteLine($"Resposta inválida, responda com (m / f)");
+                respostaCerta = false;
+                break;
+        }
     } while (respostaCerta == false);
+
+    do
+    {
+        Console.WriteLine($"Informe a sua idade: ");
+        idade = int.Parse(Console.ReadLine()!);
+
+        if (idade < 0 || idade > 100)
+        {
+            respostaCerta = false;
+            Console.WriteLine($"Idade inserida é invalida.");
+        }
+        else
+        {
+            respostaCerta = true;
+        }
+
+        if (genero == "m")
+        {
+            idadeM += idade;
+        }
+        else if (genero == "h")
+        {
+            idadeH += idade;
+        }
+
+    } while (respostaCerta == false);
+
+    do
+    {
+        Console.WriteLine($"Informe o seu peso: ");
+        peso = float.Parse(Console.ReadLine()!);
+
+        if (peso <= 0)
+        {
+            respostaCerta = false;
+            Console.WriteLine($"Peso inválido");
+        }
+        else
+        {
+            respostaCerta = true;
+        }
+    } while (respostaCerta == false);
+}
+
+
+// A.Total de Homens;      
+// B.Total de Mulheres;      
+// C.Média de idade dos Homens;      
+// D.Média de idade das mulheres.
+Console.WriteLine(@$"
+O total de homens: {h}
+O total de mulheres: {m}
+");
+
+if(h == 0)
+{
+    Console.WriteLine($"A média de idade dos homens é: 0");
+}
+else
+{
+    Console.WriteLine($"A média de idade dos homens é: {idadeH/h}");
+}
+
+if(m == 0)
+{
+    Console.WriteLine($"A média de idade das mulheres é: 0");
+}
+else
+{
+    Console.WriteLine($"A média de idade das mulheres é: {idadeM/m}");
 }
