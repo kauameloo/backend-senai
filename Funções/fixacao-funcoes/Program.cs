c0237f2da7e4639539f2c0123b9836a9a88fac3c
Kauã Rodrigues <kaua.rodrigues2620@gmail.com>
2023-04-13 17:28:28 -0300

    lista de exercicios funcoes

---
+++ Funções/fixacao-funcoes/Program.cs
@@ -0,0 +1,50 @@
+﻿//faça um método para calcular imposto sobre a renda
+
+//regras de negócio
+//tabela de imposto vs renda
+//até $1500 - isento
+//de $1501 até $3500 - 20% de imposto
+//de $3501 até $6000 - 25% de imposto
+//acima de $6000 - 35% de imposto
+
+
+//receber o renda via console
+//chamar o método passando a renda como parâmetro
+//exibir o valor do imposto referente á renda
+
+using System.Globalization;
+
+static float fImposto(float renda)
+{
+    float imposto;
+    if (renda <= 1500)
+    {
+        imposto = 0;
+    }
+    else if (renda > 1500 && renda <= 3500)
+    {
+        imposto = renda * 0.2f;
+    }
+    else if (renda > 3500 && renda <= 6000)
+    {
+        imposto = renda * 0.25f;
+    }
+    else
+    {
+        imposto = renda * 0.35f;
+    }
+    return imposto;
+}
+
+Console.WriteLine($"Informe sua renda: ");
+float rendaInformada = float.Parse(Console.ReadLine()!);
+
+
+if (fImposto(rendaInformada) == 0)
+{
+    Console.WriteLine($"Você está isento de pagar imposto");
+}
+else
+{
+    Console.WriteLine($"Você precisa pagar {fImposto(rendaInformada).ToString("C", new CultureInfo("pt-BR"))} de imposto.");
+}
---
+++ Funções/lista-de-exercicios-funcao/exercicio-2/Program.cs
@@ -0,0 +1,91 @@
+﻿//2 - Um posto está vendendo combustíveis com a seguinte tabela de descontos:
+// Álcool:
+// . até 20 litros, desconto de 3% por litro Álcool
+// . acima de 20 litros, desconto de 5% por litro
+// Gasolina:
+// . até 20 litros, desconto de 4% por litro Gasolina
+// . acima de 20 litros, desconto de 6% por litro
+
+// Escreva um algoritmo que leia o número de litros vendidos e o tipo de combustível (codificado
+// da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente
+// sabendo-se que o preço do litro da gasolina é R$ 5,30 e o preço do litro do álcool é R$ 4,90.
+// Dica: utilize switch case e funções/métodos para otimizar o algorítimo.
+
+static float Gasolina(float litros)
+{
+    float precoGasolina = 5.30f, valorPago, desconto, total = 0;
+    if (litros <= 20)
+    {
+        valorPago = precoGasolina * litros;
+        desconto = valorPago * 0.03F;
+        total = valorPago - desconto;
+    }
+    else
+    {
+        valorPago = precoGasolina * litros;
+        desconto = valorPago * 0.06F;
+        total = valorPago - desconto;
+    }
+    return total;
+}
+
+static float Alcool(float litros)
+{
+    float precoAlcool = 4.90f, valorPago, desconto, total = 0;;
+    if (litros <= 20)
+    {
+        valorPago = precoAlcool * litros;
+        desconto = valorPago * 0.04F;
+        total = valorPago - desconto;
+    }
+    else
+    {
+        valorPago = precoAlcool * litros;
+        desconto = valorPago * 0.06F;
+        total = valorPago - desconto;
+    }
+    return total;
+}
+
+char combustivel;
+bool escolhaCerta = true;
+do
+{
+    Console.WriteLine($"Informe qual é o tipo de combustível. Álcool(A) ou Gasolina(G):");
+    combustivel = char.Parse(Console.ReadLine()!.ToUpper());
+    switch (combustivel)
+    {
+        case 'A':
+            break;
+        case 'G':
+            break;
+        default:
+            Console.WriteLine($"Entrada inválida.");
+            escolhaCerta = false;
+            break;
+    }
+} while (escolhaCerta == false);
+
+float valorPago = 0;
+float litros;
+do
+{
+    Console.WriteLine($"Informe quantos litros você deseja abastecer: ");
+    litros = float.Parse(Console.ReadLine()!);
+
+    if (combustivel == 'G')
+    {
+        valorPago = Gasolina(litros);
+    }
+    else if (combustivel == 'A')
+    {
+        valorPago = Alcool(litros);
+    }
+    else
+    {
+        Console.WriteLine($"Entrada inválida.");
+        escolhaCerta = false;
+    }
+} while (escolhaCerta == false);
+
+Console.WriteLine($"O valor total a ser pago é de: {valorPago.ToString("C")}");
